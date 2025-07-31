f2775897c912305db30ef2f6cc985b63e29d2bc9
Philip <PhilipLiu@poweronlabs.ai>
2025-07-31 20:07:55 +0800

    feat: 新增project reference

--- apps/vite-project/src/App.tsx
+++ apps/vite-project/src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { components } from "@gosh/components";
+import { icons } from "@gosh/icons";
+import { utils } from "@gosh/utils";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +20,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div>{components}</div>
+      <div>{icons}</div>
+      <div>{utils}</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
--- apps/vite-project/vite.config.ts
+++ apps/vite-project/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import { fileURLToPath } from "url";
+import path from "path";
+
+// 当前模块文件的绝对路径
+const __filename = fileURLToPath(import.meta.url);
+// 当前模块目录路径，相当于 CommonJS 的 __dirname
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@gosh/components": path.join(
+        __dirname,
+        "../../gosh-libs/components/src"
+      ),
+      "@gosh/icons": path.join(__dirname, "../../gosh-libs/icons/src"),
+      "@gosh/utils": path.join(__dirname, "../../gosh-libs/utils/src"),
+    },
+  },
+});
